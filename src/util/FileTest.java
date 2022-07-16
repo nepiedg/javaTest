5f3cd0f7d709f31cea0c975fc9989071ff795e12
nepiedg <806669289@qq.com>
2022-07-16 15:35:32 +0800

    init

--- src/util/FileTest.java
+++ src/util/FileTest.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 // 文件类型
 public class FileTest {
+    private static String root = "E:/code/d3/";
+
     public static void CreatFile(String path, String filename) {
         File file = new File(path + filename);
         try {
@@ -12,6 +14,15 @@ public class FileTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void CreateDir(String path) {
+        File file = new File(root + path);
+        file.mkdir();
+    }
+
+    public static void createDirs(String path) {
+        File file = new File(root + path);
+        file.mkdirs();
     }
 }
