2f2469a770ab6a23df72ba9faf7d34547bd75397
nepiedg <806669289@qq.com>
2022-07-18 16:16:25 +0800

    add jdbc

--- src/Start.java
+++ src/Start.java
@@ -1,15 +1,18 @@
+import util.ArrayTest; 
 import util.FileTest;
+import util.Odbc;
 import util.Times;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
 // 类型转换
 import util.TypeConvert;
+
 // https://blog.csdn.net/gfd54gd5f46/article/details/54918168
 public class Start {
     public static void main(String[] args) {
-        FileTest.CreatFile("E:/code/d3/", "java.txt");
+        Odbc.testSql();
     }
 
 }
